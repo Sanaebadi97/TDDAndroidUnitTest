62c2a523f8335c4c48ba5e095a038ed91787b46a
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-17 06:47:29 -0400

    added data class

---
+++ app/src/main/java/info/sanaebadi/tddandroidunittest/model/Note.kt
@@ -0,0 +1,25 @@
+package info.sanaebadi.tddandroidunittest.model
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "notes")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+
+    @NonNull
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    @ColumnInfo(name = "content")
+    val content: String,
+
+    @ColumnInfo(name = "timestamp")
+    val timestamp: String
+)
+
+
