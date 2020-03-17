903e2ac4af6eb65dd6af8bb2a5a3a1ec0de1b4ac
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-17 08:31:13 -0400

    added command to note test class for start test junit 5

---
+++ app/src/test/java/info/sanaebadi/tddandroidunittest/model/NoteTest.kt
@@ -0,0 +1,43 @@
+package info.sanaebadi.tddandroidunittest.model
+
+import org.junit.jupiter.api.Test
+
+class NoteTest {
+
+    /*
+
+    Compare two equal   Notes
+     */
+    @Test
+    internal fun name() {
+        TODO("Not yet implemented")
+    }
+
+    /*
+
+    Compare notes with 2 different  ids
+     */
+
+
+    /*
+
+    Compare two notes with different timestamp
+
+     */
+
+
+    /*
+
+    Compare two notes with different title
+     */
+
+
+
+    /*
+
+    Compare two notes with different content
+     */
+
+
+
+}
