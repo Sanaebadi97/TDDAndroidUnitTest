79aefcfdc6a4efcec2b106a5a06e67b8c7aa1339
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-12 12:11:19 -0400

    created App module and created 2 functions

--- app/src/main/java/info/sanaebadi/tddandroidunittest/di/AppModule.kt
+++ app/src/main/java/info/sanaebadi/tddandroidunittest/di/AppModule.kt
@@ -1,8 +1,31 @@
 package info.sanaebadi.tddandroidunittest.di
 
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
+import info.sanaebadi.tddandroidunittest.persistence.NoteDao
+import info.sanaebadi.tddandroidunittest.persistence.NoteDatabase
+import javax.inject.Singleton
 
 @Module
 class AppModule {
+
+    @Singleton
+    @Provides
+
+    fun provideNoteDatabase(application: Application): NoteDatabase? {
+        return Room.databaseBuilder(
+            application,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideNoteDao(noteDatabase: NoteDatabase): NoteDao {
+        return noteDatabase.gtNoteDao()
+    }
 
 }
--- app/src/main/java/info/sanaebadi/tddandroidunittest/persistence/NoteDatabase.kt
+++ app/src/main/java/info/sanaebadi/tddandroidunittest/persistence/NoteDatabase.kt
@@ -1,10 +1,11 @@
 package info.sanaebadi.tddandroidunittest.persistence
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import info.sanaebadi.tddandroidunittest.model.Note
 
 @Database(entities = [Note::class], version = 1)
-abstract class NoteDatabase {
+abstract class NoteDatabase : RoomDatabase() {
 
     companion object {
         const val DATABASE_NAME = "notes_db"
