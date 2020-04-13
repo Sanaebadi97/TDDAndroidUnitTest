0afab82040994bd345e108356cd91ce47563540f
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-13 13:25:27 -0400

    added NoteDaoTest class

---
+++ app/src/androidTest/java/info/sanaebadi/tddandroidunittest/NoteDaoTest.kt
@@ -0,0 +1,18 @@
+package info.sanaebadi.tddandroidunittest
+
+class NoteDaoTest : NoteDatabaseTest() {
+
+
+    // Insert , Read , Delete
+
+
+    //Insert , Read , Update , Delete
+
+
+    //Insert Note with null title throw exception
+
+
+    //Insert , Update  Note with null title throw exception
+
+
+}
